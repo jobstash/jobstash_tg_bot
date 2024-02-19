2ac0ffd8fe3c11d4449b384afae449299fa13819
defuera <justdeveloper@gmail.com>
2024-02-19 18:16:26 +0100

    Update thread

--- packages/ai_assistant/lib/src/ai_service.dart
+++ packages/ai_assistant/lib/src/ai_service.dart
@@ -22,7 +22,7 @@ class AiAssistant {
       "user_input": tags,
     });
 
-    final response = await _assistantApi.addMessageToThread('all_users_thread_0', prompt);
+    final response = await _assistantApi.addMessageToThread('all_users_thread_1', prompt);
 
     return _parseResponse(
       response,
